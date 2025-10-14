324302748cab067ef6a3b5ce0b1f0e271648dd67
hariprasathselvamani <hariprasaths1331@gmail.com>
2025-10-14 17:05:36 +0530

    fix in nodemailer change port and set timeout

--- server/Config/nodemailer.js
+++ server/Config/nodemailer.js
@@ -2,10 +2,12 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: "587",
+  port: "465",
+  secure: true,
   auth: {
     user: "95145e001@smtp-brevo.com",
     pass: "v3TCGRy1JcSnwjrK",
   },
+  timeout: 10000,
 });
 export default transporter;
