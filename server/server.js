70119d5ed92d9c91481b98b1949c7b4170073ae0
hariprasathselvamani <hariprasaths1331@gmail.com>
2025-10-14 12:00:38 +0530

    Fix in server folder server.js for render deployment

--- server/server.js
+++ server/server.js
@@ -2,34 +2,43 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import authRouter from "./routes/authRoutes.js";
-import connectDB from "./Config/mongodb.js";
 import userRouter from "./routes/userRoutes.js";
+import connectDB from "./Config/mongodb.js";
+import path, { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 connectDB();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigins = [
-  process.env.FRONTEND_URL,
-].filter(Boolean);
+const allowedOrigins = [process.env.FRONTEND_URL].filter(Boolean);
 app.use(
   cors({
     origin: allowedOrigins,
     credentials: true,
   })
-); // adjust frontend URL
+);
 
 app.get("/", (req, res) => res.send("API Working"));
-
-
 
 // Mount routes
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 
 app.use((req, res, next) => {
-    res.status(404).json({ success: false, message: "API route not found" });
+  res.status(404).json({ success: false, message: "API route not found" });
+});
+
+// Serve React client
+app.use(express.static(join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(join(__dirname, "client/build", "index.html"));
 });
 
 const PORT = process.env.PORT || 4000;
