8dd1682f62c43a8787a94964e02df01b4d0c3767
hariprasathselvamani <hariprasaths1331@gmail.com>
2025-10-22 20:07:00 +0530

    fix in server config and nodemailer.js

--- server/Config/nodemailer.js
+++ server/Config/nodemailer.js
@@ -2,11 +2,11 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: "465",
-  secure: true,
+  port: "587",
+  secure: false,
   auth: {
-    user: "95145e001@smtp-brevo.com",
-    pass: "v3TCGRy1JcSnwjrK",
+    user: process.SMTP_USER,
+    pass: process.SMTP_PASS,
   },
   timeout: 10000,
 });
